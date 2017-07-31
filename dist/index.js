46681c7ad5c243460c29a77b4ef1d4443d1b06d9
Charles <charles_candelaria@trimble.com>
2017-07-31 12:38:31 -0600

    adds source kafka + topic verifying

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,68 @@
 
 var _settings = require('./settings');
 
+var _chalk = require('chalk');
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _kafkaNode = require('kafka-node');
+
+var _lodash = require('lodash');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 (function () {
   var kafkaSource = void 0;
   var kafkaDest = void 0;
 
-  new _settings.KafkaSettings().prompt().then(function (src) {
-    kafkaSource = src;
-    return new _settings.KafkaSettings().prompt();
-  }).then(function (dest) {
+  console.log(_chalk2.default.yellow('========================================================'));
+  console.log(_chalk2.default.yellow('Source Kafka Settings...'));
+  console.log(_chalk2.default.yellow('========================================================'));
+
+  getSource().then(function (src) {
+    return kafkaSource = src;
+  }).then(getDestination).then(function (dest) {
     kafkaDest = dest;
   }).then(function () {
     return console.log({ kafkaDest: kafkaDest, kafkaSource: kafkaSource });
   }).catch(console.log);
 })();
+
+function getSource() {
+  return new _settings.KafkaSettings().prompt().then(validateTopic);
+}
+
+function getDestination() {
+  console.log(_chalk2.default.yellow('========================================================'));
+  console.log(_chalk2.default.yellow('Destination Kafka Settings...'));
+  console.log(_chalk2.default.yellow('========================================================'));
+
+  return new _settings.KafkaSettings().prompt();
+}
+
+function validateTopic(kafkaSource) {
+  var client = new _kafkaNode.Client(kafkaSource.host);
+  var topic = void 0;
+
+  return new Promise(function (resolve, reject) {
+    client.once('connect', function () {
+      client.loadMetadataForTopics([], function (err, results) {
+        if (err) {
+          throw err;
+        }
+
+        var topicFound = (0, _lodash.get)(results, '1.metadata')[kafkaSource.topic];
+
+        if (!topicFound) {
+          reject(_chalk2.default.red('\nThe topic was not found. Please re-enter the source kafka info.\n'));
+        }
+        console.log(_chalk2.default.bgGreen('--- Kafka Settings valid ---'));
+        console.log(_chalk2.default.green(kafkaSource.topic + ' found!'));
+        resolve(kafkaSource);
+      });
+    });
+  }).catch(function (mess) {
+    console.log(mess);
+    return getSource();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { KafkaSettings } from './settings'; 
-
+import chalk from 'chalk';
 
 
 (function() {
   let kafkaSource;
   let kafkaDest;
 
-  new KafkaSettings().prompt()
-    .then( src => {
-      kafkaSource = src;
-      return new KafkaSettings().prompt();
-    })  
+  console.log(chalk.yellow(`========================================================`));
+  console.log(chalk.yellow(`Source Kafka Settings...`));
+  console.log(chalk.yellow(`========================================================`));
+
+  getSource()
+    .then(src => kafkaSource = src)
+    .then(getDestination)  
     .then( dest => {
       kafkaDest = dest;
     })
@@ -18,3 +20,47 @@ import { KafkaSettings } from './settings';
     .catch(console.log)
 
 })();
+
+function getSource() {
+  return new KafkaSettings().prompt()
+    .then(validateTopic)
+}
+
+function getDestination() {
+  console.log(chalk.yellow(`========================================================`));
+  console.log(chalk.yellow(`Destination Kafka Settings...`));
+  console.log(chalk.yellow(`========================================================`));
+
+  return new KafkaSettings().prompt();
+}
+
+import { Client } from 'kafka-node';
+import { get } from 'lodash';
+
+function validateTopic(kafkaSource) {
+  const client = new Client(kafkaSource.host);
+  let topic;
+
+  return new Promise((resolve, reject) => {
+    client.once('connect', () => {
+      client.loadMetadataForTopics([], function( err, results) {
+        if(err) {
+          throw err;
+        }
+
+        let topicFound = get(results, '1.metadata')[kafkaSource.topic];
+
+        if(!topicFound) {
+          reject(chalk.red(`\nThe topic was not found. Please re-enter the source kafka info.\n`));
+        }
+        console.log(chalk.bgGreen(`--- Kafka Settings valid ---`));
+        console.log(chalk.green(`${kafkaSource.topic} found!`));
+        resolve(kafkaSource);
+      });
+    });
+  })
+  .catch( mess => {
+    console.log(mess);
+    return getSource();
+  });
+}
